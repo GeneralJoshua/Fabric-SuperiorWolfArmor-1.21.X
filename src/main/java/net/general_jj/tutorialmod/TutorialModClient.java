6081252474e627c7d2e7c98ea30717400a3727bc
GeneralJoshua <superyummymango@gmail.com>
2025-04-12 12:49:41 -0700

    Initial Commit

---
+++ src/main/java/net/general_jj/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.general_jj.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
